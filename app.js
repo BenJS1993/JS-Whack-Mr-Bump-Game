d77614caaba9a0402b8e48274851e43d950c02df
Code Nation 13 <codenation@CN-13.local>
2019-04-10 14:54:45 +0100

    updates

--- app.js
+++ app.js
@@ -1,3 +1,46 @@
-const holes = document.querySelectorAll('.hole');
-const scoreBoard = document.querySelector('.score');
-const moles = document.querySelectorAll('.mole');
+// const holes = document.querySelectorAll(".hole");
+// const scoreBoard = document.querySelector(".score");
+// const moles = document.querySelectorAll(".mole");
+
+function timer(min, max) {
+  return Math.round(Math.random() * (max - min) + min);
+}
+
+function randomHole(field) {
+  const idx = Math.floor(Math.random() * field.length);
+  const hole = field[idx];
+  if (hole == lastHole) {
+    return randomHole(field);
+  }
+  lastHole = hole;
+  return hole;
+}
+
+function surprise() {
+  const time = randomTime(500, 1200);
+  const hole = randomHole(field);
+  hole.classList.add("up");
+  setTimeout(() => {
+    hole.classList.remove("up");
+    if (!timeUp) surprise();
+  }, time);
+}
+
+function countDown() {
+  seconds = document.getElementById("countdown").innerHTML;
+
+  if (seconds == 0) {
+    temp.innerHTML = "Buzzzz!!";
+    if (score > highScore) {
+      highest.textContent = score;
+      localStorage.setItem("High Score", score);
+      alert(
+        `You've set a new high score! You whacked Mr. Bump ${score} times!`
+      );
+    }
+    return;
+  }
+  seconds--;
+  temp.innerHTML = seconds;
+  setTimeout(countDown, 1000);
+}
